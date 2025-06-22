4118a6a28dbbce524b71ca47a1d5eac5ae66e0b9
RedaMacbook <Reda.f95@hotmail.com>
2025-06-22 22:37:56 +0200

    api connection code written. Connected to paper trading platform

--- algorithmicTrading/indiator_1.py
+++ algorithmicTrading/indiator_1.py
@@ -1,2 +1,21 @@
 # @author: Reda Benhci (https://github.com/Reda-Benchi/projects/tree/main/algorithmicTrading)
 
+from ibapi.client import EClient 
+# establish connection to IBKR through your python script
+
+from ibapi.wrapper import EWrapper
+# EWrapper is an interface that contains methods to handle events from IBKR
+
+class TradingApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self,self)
+
+    def error(self, reqId, errorCode, errorString, advancedOrderRejectJson=""):
+        print("Error. Id: {}\nCode: {}\nMsg: {}\n".format(reqId, errorCode, errorString))
+    # This method is called when an error occurs. It prints the error message.
+
+app = TradingApp()
+app.connect("127.0.0.1", 7497, 1)
+# Connect to the IBKR TWS or Gateway. The parameters are host, port, and
+
+app.run()
